d10a0cabc696218577e127d2eda5f9cedd9d2b04
k-minyoung <alsdudsk12@naver.com>
2023-10-18 19:47:52 +0900

    bugfix4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './store/cart.js'
 import { Provider } from 'react-redux'
