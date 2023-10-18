35b727e186cee0f3d4bc7bdb044517f288c4353c
k-minyoung <alsdudsk12@naver.com>
2023-10-18 21:01:38 +0900

    test4

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        createProxyMiddleware('/v1/search', {
+        createProxyMiddleware('/v1/search/shop.json', {
             target: 'https://openapi.naver.com',
             changeOrigin: true,
         }),
